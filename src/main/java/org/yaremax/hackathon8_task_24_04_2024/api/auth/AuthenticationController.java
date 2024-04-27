c22c5cafa29b06543da8c9f1be55f47a5cdeda42
Maksym Yarema <macsym2@gmail.com>
2024-04-27 15:48:28 +0300

    add temp CrossOrigin for  "http://localhost:5173"

--- src/main/java/org/yaremax/hackathon8_task_24_04_2024/api/auth/AuthenticationController.java
+++ src/main/java/org/yaremax/hackathon8_task_24_04_2024/api/auth/AuthenticationController.java
@@ -2,10 +2,7 @@ package org.yaremax.hackathon8_task_24_04_2024.api.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.yaremax.hackathon8_task_24_04_2024.api.auth.records.RegisterRequest;
 import org.yaremax.hackathon8_task_24_04_2024.api.auth.records.AuthenticationResponse;
 import org.yaremax.hackathon8_task_24_04_2024.api.auth.records.LoginRequest;
@@ -13,6 +10,7 @@ import org.yaremax.hackathon8_task_24_04_2024.api.auth.records.LoginRequest;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
