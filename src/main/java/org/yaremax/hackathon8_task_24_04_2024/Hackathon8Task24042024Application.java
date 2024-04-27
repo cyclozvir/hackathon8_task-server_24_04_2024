3f478063a4e6cb064f909f484dcdc963a8fb9b23
Maksym Yarema <macsym2@gmail.com>
2024-04-27 11:42:11 +0300

    init project

---
+++ src/main/java/org/yaremax/hackathon8_task_24_04_2024/Hackathon8Task24042024Application.java
@@ -0,0 +1,13 @@
+package org.yaremax.hackathon8_task_24_04_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon8Task24042024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackathon8Task24042024Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/yaremax/hackathon8_task_24_04_2024/Hackathon8Task24042024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yaremax.hackathon8_task_24_04_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon8Task24042024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
