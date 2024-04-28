e8aa661a2a44ceec623e5aad5f0c1e7d16f60022
Maksym Yarema <macsym2@gmail.com>
2024-04-28 14:51:15 +0300

    feat: move jwt secret key to env var

--- src/main/java/org/yaremax/hackathon8_task_24_04_2024/jwt/JwtService.java
+++ src/main/java/org/yaremax/hackathon8_task_24_04_2024/jwt/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,8 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "6hLxNfCQTj8Ftmp+iXbi4uAAlAHUkCUYpU58E0amPAA=";
+    @Value("${app.config.security.jwt.secret_key}")
+    private String SECRET_KEY;
 
     public String extractSubject(String token) {
         return extractClaim(token, Claims::getSubject);
